22879a9909010c76998ac8dd26049beb8bd6f4d2
tjona <m. brambilla@presstoday.com>
2022-01-04 15:20:25 +0100

    Esempio di for loop

---
+++ foreach/src/test/java/com/example/foreach/ForeachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foreach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
